4594c169641259f6f1e640108d922f738fbbd389
Pavel Savenkov <zybastik-02@mail.ru>
2015-11-12 21:03:11 +0300

    20

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function() {
+
+    
+	/*
+		Preloader
+	*/
+	
+  	$(window).load(function() {
+
+   	// will first fade out the loading animation 
+    	$("#status").fadeOut("slow"); 
+
+    	// will fade out the whole DIV that covers the website. 
+    	$("#preloader").delay(500).fadeOut("slow").remove();      
+
+  	}) 
+    });
